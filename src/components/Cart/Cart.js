9090cb30ed5e89c2d7d33e17faf6226e91d675b5
danakal <dana.kalcikova@centrum.cz>
2023-04-07 14:54:52 +0200

    useContext and useRef in Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,13 @@
+//import { useContext } from 'react';
+
+//import CartContext from '../../store/cart-context';
+
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
 const Cart = (props) => {
+ // const cartCtx = useContext(CartContext);
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
---
+++ src/store/CartProvider.js
@@ -0,0 +1,42 @@
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM'){
+        const newItems = [...state.items, action.item];
+        const newTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {items: newItems, totalAmount: newTotalAmount}
+    }
+    return defaultCartState;
+}
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD_ITEM', item: item})
+    };
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE_ITEM', id: id})
+    };
+
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+    };
+
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+}
+
+export default CartProvider;
