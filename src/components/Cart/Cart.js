5acbecfbcfc1171cfc4dce50043b36092ff5102d
danakal <dana.kalcikova@centrum.cz>
2023-04-07 08:54:13 +0200

    opening/closing Cart modal component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,14 +11,14 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>39.45</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
